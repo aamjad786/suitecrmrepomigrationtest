50fc082e6ab8b260b558890c80f604cab0580620
Aamjad Pinjari <developers@nucsoft.com>
2021-09-27 11:49:08 +0530

    Bean File changes revert

--- custom/Extension/application/Ext/Include/customLead.php
+++ custom/Extension/application/Ext/Include/customLead.php
@@ -1,12 +1,12 @@
 <?php
 
 
-// $objectList['Leads'] = 'Lead';
+$objectList['Leads'] = 'Lead';
 
-// // $beanList maps the Bean/Module name to the Class name
-// $beanList['Leads'] = 'CustomLead';
+// $beanList maps the Bean/Module name to the Class name
+$beanList['Leads'] = 'CustomLead';
 
-// // $beanFiles maps the Class name to the PHP Class file
-$beanFiles['Lead'] = 'custom/modules/Leads/CustomLead.php';
+// $beanFiles maps the Class name to the PHP Class file
+$beanFiles['CustomLead'] = 'custom/modules/Leads/CustomLead.php';
 
-// $dictionary['CustomLead']=array();
+$dictionary['CustomLead']=array();
