c154cd8da1d9168a70bd39038941ddba56d97839
Aamjad Pinjari <developers@nucsoft.com>
2021-10-01 10:46:51 +0530

    Leads Fixes

---
+++ custom/Extension/modules/Leads/Ext/Vardefs/_override_sugarfield_sent_count_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2021-09-30 14:30:05
+$dictionary['CustomLead']['fields']['sent_count_c']['inline_edit']='1';
+$dictionary['CustomLead']['fields']['sent_count_c']['labelValue']='No. of times Lead successfully pushed to EOS';
+
+ ?>
