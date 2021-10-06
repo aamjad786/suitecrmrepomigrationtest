ca63a8cc8dbc778c3641cfc94829c97c427d2f07
Nikita Patil <nikita_patil@nucsoft.in>
2021-10-06 18:08:52 +0530

    holiday list module

---
+++ custom/modules/scrm_Holidays_List/metadata/listviewdefs.php
@@ -0,0 +1,35 @@
+<?php
+$module_name = 'scrm_Holidays_List';
+$listViewDefs [$module_name] = 
+array (
+  'NAME' => 
+  array (
+    'width' => '32%',
+    'label' => 'LBL_NAME',
+    'default' => true,
+    'link' => true,
+  ),
+  'HOLIDAY_DATE' => 
+  array (
+    'type' => 'date',
+    'label' => 'LBL_HOLIDAY_DATE',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'DATE_ENTERED' => 
+  array (
+    'type' => 'datetime',
+    'label' => 'LBL_DATE_ENTERED',
+    'width' => '10%',
+    'default' => true,
+  ),
+  'ASSIGNED_USER_NAME' => 
+  array (
+    'width' => '9%',
+    'label' => 'LBL_ASSIGNED_TO_NAME',
+    'module' => 'Employees',
+    'id' => 'ASSIGNED_USER_ID',
+    'default' => true,
+  ),
+);
+?>
