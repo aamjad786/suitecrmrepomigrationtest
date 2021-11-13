3b5720d455660ad96c64b9fcc85c88664fb1f369
Pallavi <pallavi>
2021-11-13 11:03:51 +0530

    re assigned to popup fix

--- custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_reassigned_user_id_c.php
+++ custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_reassigned_user_id_c.php
@@ -3,5 +3,5 @@
 $dictionary['Case']['fields']['reassigned_user_id_c']['inline_edit']=1;
 $dictionary['Case']['fields']['reassigned_user_id_c']['duplicate_merge_dom_value']='0';
 $dictionary['Case']['fields']['reassigned_user_id_c']['merge_filter']='disabled';
-
+$dictionary['Case']['fields']['reassigned_user_id_c']['module']='Users';
  ?>
