c18b7380ffe355c829c94563d18019dbf241961d
Pallavi <pallavi>
2021-11-24 19:38:46 +0530

    missing fields added

---
+++ custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_pan_no_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2016-09-16 11:42:36
+$dictionary['Account']['fields']['pan_no_c']['inline_edit']='1';
+$dictionary['Account']['fields']['pan_no_c']['labelValue']='Pan No.';
+
+ ?>
---
+++ custom/Extension/modules/Contacts/Ext/Vardefs/sugarfield_personal_email_c.php
@@ -0,0 +1,6 @@
+<?php
+ // created: 2016-09-17 14:38:37
+$dictionary['Account']['fields']['personal_email_c']['inline_edit']='1';
+$dictionary['Account']['fields']['personal_email_c']['labelValue']='Personal Email';
+
+ ?>
