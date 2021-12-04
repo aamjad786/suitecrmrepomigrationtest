760ec057b481f52afa8ce289a6c38cc23695bb59
Pallavi <pallavi>
2021-12-04 16:12:42 +0530

    validation msgs

--- custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_summary_c.php
+++ custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_summary_c.php
@@ -5,11 +5,11 @@ $dictionary['Case']['fields']['summary_c']['massupdate']=true;
 $dictionary['Case']['fields']['summary_c']['validation'] = array (
     'type' => 'callback',
     'callback' => 'function(formname, nameIndex) {
-        var regEx=/^[a-zA-Z ]*$/;
+        var regEx=/^[ A-Za-z0-9_.-_-:\/&=.<>()|?#,;"%]*$/;
         var value=$("#" + nameIndex).val();
         //console.log("field value"+value);
         if (regEx.test(value)== false) {
-            add_error_style(formname, nameIndex, "Invalid Value.Special characters and numbers are not allowed.");
+            add_error_style(formname, nameIndex, "Invalid Value. A-Z, a-z, 0-9 and &=.<>()|?#,;"%\/:-.-_ allowed only.");
             return false;
         };
         return true;
--- custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_description.php
+++ custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_description.php
@@ -4,17 +4,17 @@ $dictionary['Case']['fields']['description']['inline_edit']=true;
 $dictionary['Case']['fields']['description']['comments']='Full text of the note';
 $dictionary['Case']['fields']['description']['merge_filter']='disabled';
 $dictionary['Case']['fields']['description']['massupdate']=false;
-/*$dictionary['Case']['fields']['description']['validation'] = array (
+$dictionary['Case']['fields']['description']['validation'] = array (
     'type' => 'callback',
     'callback' => 'function(formname, nameIndex) {
-        var regEx=/^[ A-Za-z0-9_.-_-:/&=.<>()|?#,;"%]*$/;
+        var regEx=/^[ A-Za-z0-9_.-_-:\/&=.<>()|?#,;"%]*$/;
         var value=$("#" + nameIndex).val();
         //console.log("field value"+value);
         if (regEx.test(value)== false) {
-            add_error_style(formname, nameIndex, "Invalid Value.Some Special characters are not allowed.");
+            add_error_style(formname, nameIndex, "Invalid Value. A-Z, a-z, 0-9 and &=.<>()|?#,;"%\/:-.-_ allowed only.");
             return false;
         };
         return true;
     }',
-);*/
+);
  ?>
