6808c67625deccb6b9f5c54a4fd072f4783bc968
Pallavi <pallavi>
2021-12-04 12:53:14 +0530

    case owner mass update  change

--- custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_attended_by_c.php
+++ custom/Extension/modules/Cases/Ext/Vardefs/sugarfield_attended_by_c.php
@@ -2,8 +2,7 @@
  // created: 2018-08-30 11:48:34
 $dictionary['Case']['fields']['attended_by_c']['inline_edit']='1';
 $dictionary['Case']['fields']['attended_by_c']['labelValue']='Case Owner';
-$dictionary['Case']['fields']['attended_by_c']['massupdate']=true;
-$dictionary['Case']['fields']['attended_by_c']['validation'] = array (
+/*$dictionary['Case']['fields']['attended_by_c']['validation'] = array (
     'type' => 'callback',
     'callback' => 'function(formname, nameIndex) {
         var regEx=/^[ A-Za-z0-9_-]*$/;
@@ -13,7 +12,8 @@ $dictionary['Case']['fields']['attended_by_c']['validation'] = array (
             add_error_style(formname, nameIndex, "Invalid Value.Some Special characters are not allowed.");
             return false;
         };
+        
         return true;
     }',
-);
+);*/
  ?>
