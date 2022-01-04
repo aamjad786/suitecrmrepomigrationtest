70cf51bd1372723d0babc48d9cf1147dc1dfa3c8
Nucsoft Developer <developers@nucsoft.com>
2022-01-04 14:40:26 +0530

    from master: cases validation fix

--- custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_merchant_contact_number_c.php
+++ custom/Extension/modules/Cases/Ext/Vardefs/_override_sugarfield_merchant_contact_number_c.php
@@ -5,12 +5,12 @@ $dictionary['Case']['fields']['merchant_contact_number_c']['labelValue']='Mercha
 $dictionary['Case']['fields']['merchant_contact_number_c']['validation'] = array (
     'type' => 'callback',
     'callback' => 'function(formname, nameIndex) {
-        var regEx=/^[0-9]{10}$/;
+        var regEx=/^[[anAN_/]{3}]*$|^[[anAN]{2}]*$|^[0-9]{10}$/;
         var value=$("#" + nameIndex).val();
-        //console.log("field value"+value);
+       // console.log("field value"+value);
         if (regEx.test(value)== false) {
-            add_error_style(formname, nameIndex, "Please Enter Valid Mobile Number!");
-            return false;
+           add_error_style(formname, nameIndex, "Please Enter Valid Mobile Number!");
+           return false;
         };
         return true;
     }',
